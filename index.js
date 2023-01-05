5fe8669503db05f5a0f5de041634da637d8ad9bf
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2023-01-05 09:04:21 -0500

    firtst commi

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+const port = 8000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(port, function () {
+  // var host = server.address().address;
+  // var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
